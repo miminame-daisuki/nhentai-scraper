0b7d216c65b094951918f8b6caa2e6f46313991f
Miminame-Daisuki <barrier.lectern0f@icloud.com>
2025-03-20 09:05:54 +0900

    Changed t5 and t7 to t1 and t4 for 'THUMB_BASE_URL', and deleted unneeded code

--- src/nhentai_urls.py
+++ src/nhentai_urls.py
@@ -3,10 +3,10 @@ FAVORITES_URL = 'https://nhentai.net/favorites'
 API_SEARCH_URL = 'https://nhentai.net/api/galleries/search'
 API_GALLERY_URL = 'https://nhentai.net/api/gallery'
 
+THUMB_BASE_URL_t1 = 'https://t1.nhentai.net/galleries'
 THUMB_BASE_URL_t2 = 'https://t2.nhentai.net/galleries'
 THUMB_BASE_URL_t3 = 'https://t3.nhentai.net/galleries'
-THUMB_BASE_URL_t5 = 'https://t5.nhentai.net/galleries'
-THUMB_BASE_URL_t7 = 'https://t7.nhentai.net/galleries'
+THUMB_BASE_URL_t4 = 'https://t4.nhentai.net/galleries'
 
 IMG_BASE_URL_i2 = 'https://i2.nhentai.net/galleries'
 IMG_BASE_URL_i3 = 'https://i3.nhentai.net/galleries'
