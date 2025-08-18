33cb8c97f3f3214cd2efadfddf3f8cc2265eb1fa
Miminame-Daisuki <barrier.lectern0f@icloud.com>
2025-08-19 00:51:38 +0900

    Added several arguments

--- src/cli.py
+++ src/cli.py
@@ -7,7 +7,7 @@ def cli_parser():
 
     parser.add_argument(
         "--update-cookies",
-        help="Update cookies used for bypassing Cloudflare protection",
+        help="Update cookies used for bypassing Cloudflare protection.",
         action="store_true",
     )
     parser.add_argument(
@@ -15,10 +15,27 @@ def cli_parser():
         help="Confirm various settings: download location, ...",
         action="store_true",
     )
+    parser.add_argument(
+        "--skip-to-tag",
+        help="Start download at the specified tag, skipping all previous tags in 'download_list.txt'",
+    )
+    parser.add_argument(
+        "--thumbnail",
+        help="Set thumbnails to each downloaded gallery.",
+        action="store_true",
+        default=True,
+    )
+    parser.add_argument(
+        "--tags",
+        help="Set tags to each downloaded gallery.",
+        action="store_true",
+        default=True,
+    )
 
     args = parser.parse_args()
 
     return args
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = cli_parser()
