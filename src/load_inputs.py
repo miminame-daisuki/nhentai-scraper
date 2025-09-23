51ff5d6d7d387227a0b3e7974571913505ad63f9
Miminame-Daisuki <barrier.lectern0f@icloud.com>
2025-09-23 12:46:06 +0900

    Added cli_parser() so that setting will only be prompted when providing command line arguments

--- src/load_inputs.py
+++ src/load_inputs.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import json
 from typing import Union, Optional
 
+import cli
 import misc
 
 
@@ -75,39 +76,45 @@ def confirm_settings() -> dict:
 
     settings = {}
 
+    # load command line arguments
+    args = cli.cli_parser()
+
     # confirm download location
     application_folder_path = misc.get_application_folder_dir()
     download_dir = str(misc.set_download_dir())
-    while True:
-        confirm_download_dir = input((f'Download to {download_dir}?(y/n)'))
-        if confirm_download_dir != 'y':
-            download_dir = input('Download directory: ')
-            download_dir = misc.set_download_dir(download_dir)
-        else:
-            break
+    if args.confirm_settings:
+        while True:
+            confirm_download_dir = input((f'Download to {download_dir}?(y/n)'))
+            if confirm_download_dir != 'y':
+                download_dir = input('Download directory: ')
+                download_dir = misc.set_download_dir(download_dir)
+            else:
+                break
     settings['download_dir'] = download_dir
 
     # create `cookies.json` and `headers.json` if not present in `inputs/`
-    inputs_path = Path(f'{application_folder_path}/inputs').absolute()
-    if 'cookies.json' not in [file.name for file in inputs_path.iterdir()]:
-        write_cookies(inputs_path)
-    else:
-        x = input('Update cookies? (y/n)')
-        if x != 'n':
+    if args.confirm_settings:
+        inputs_path = Path(f'{application_folder_path}/inputs').absolute()
+        if 'cookies.json' not in [file.name for file in inputs_path.iterdir()]:
             write_cookies(inputs_path)
-    if 'headers.json' not in [file.name for file in inputs_path.iterdir()]:
-        write_headers(inputs_path)
+        else:
+            x = input('Update cookies? (y/n)')
+            if x != 'n':
+                write_cookies(inputs_path)
+        if 'headers.json' not in [file.name for file in inputs_path.iterdir()]:
+            write_headers(inputs_path)
 
     check_fileicon_tag_install()
 
-    skip_downloaded_ids = input('Skip downloaded galleries?(y/n)')
-    if skip_downloaded_ids == 'y':
-        skip_downloaded_ids = True
-    else:
-        skip_downloaded_ids = False
-    settings['skip_downloaded_ids'] = skip_downloaded_ids
+    if args.confirm_settings:
+        skip_downloaded_ids = input('Skip downloaded galleries?(y/n)')
+        if skip_downloaded_ids == 'y':
+            skip_downloaded_ids = True
+        else:
+            skip_downloaded_ids = False
+        settings['skip_downloaded_ids'] = skip_downloaded_ids
 
-    settings['skip_to_tag'] = input('Skip to tag?(Press Enter for no skip)')
+        settings['skip_to_tag'] = input('Skip to tag?(Press Enter for no skip)')
 
     print('-'*os.get_terminal_size().columns)
 
